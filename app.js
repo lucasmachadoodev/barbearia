91aa1f0c505dcdef53c1e363dd6d9190dd83d161
Lucas Machado da Silva <155257005+lucasmachadoodev@users.noreply.github.com>
2024-04-22 20:12:57 -0300

    barbearia

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function alert(){
+
+    Swal.fire({
+        title: "Agendamento concluído!",
+        text: "Obrigado pela preferência!",
+        icon: "success",
+        showConfirmButton: true,
+        timer: 4500
+      });
+}
